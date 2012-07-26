907be85e790800ad225aa11f6cc8c112d4147c01
Blueharford <blueharford@gmail.com>
2012-07-26 08:41:12 -0700

    fix derp
    Change-Id: I150747440bc0e6fe3dfeffe4e3fadeeeb39fa6e5

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,16 +1,10 @@
 # Inherit common ADA stuff
 $(call inherit-product, vendor/ada/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 ifeq ($(TARGET_BOOTANIMATION_NAME),)
     PRODUCT_COPY_FILES += \
-        vendor/ada/prebuilt/common/bootanimation/horizontal-1280x800.zip:system/media/bootanimation.zip
+        vendor/ada/prebuilt/common/bootanimation/bootanimation_n7.zip:system/media/bootanimation.zip
 endif
