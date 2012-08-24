54f3ef4eb01e61d8aa855e72467b2f46d1e8cd41
Hiemanshu Sharma <hiemanshu@gmail.com>
2012-08-24 09:58:51 -0500

    Add tf700t and tf101
    Change-Id: I135e4ac65acc8609940d14a63ee76eab13654dc2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ add_lunch_combo ada_maguro-userdebug
 add_lunch_combo ada_toro-userdebug
 #add_lunch_combo ada_toroplus-userdebug
 add_lunch_combo ada_n7000-userdebug
+add_lunch_combo ada_tf700t-userdebug
+add_lunch_combo ada_tf101-userdebug
