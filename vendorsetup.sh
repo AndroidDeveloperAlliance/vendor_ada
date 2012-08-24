9c81d4728db2f91eb661002ae0976b7fa38b518f
blueharford <blueharford@gmail.com>
2012-08-24 13:46:17 -0500

    cleanup and add toroplus
    Change-Id: I81c273f05bab9308ce01a2064eff7fcc7d384d84

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,7 @@
 add_lunch_combo ada_grouper-userdebug
 add_lunch_combo ada_maguro-userdebug
-#add_lunch_combo ada_tf101-userdebug
 add_lunch_combo ada_toro-userdebug
-#add_lunch_combo ada_toroplus-userdebug
+add_lunch_combo ada_toroplus-userdebug
 add_lunch_combo ada_n7000-userdebug
-add_lunch_combo ada_tf700t-userdebug
 add_lunch_combo ada_tf101-userdebug
+add_lunch_combo ada_tf700t-userdebug
