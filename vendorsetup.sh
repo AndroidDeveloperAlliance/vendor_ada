39c34d1414d82d4debacca8edbbf93c9d80ab3bd
blueharford <blueharford@gmail.com>
2012-09-04 16:52:47 -0500

    add i9100
    Change-Id: I032c510b44def3ee207d6240abc7bf02556031c5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo ada_d710-userdebug
 add_lunch_combo ada_grouper-userdebug
+add_lunch_combo ada_i9100-userdebug
 add_lunch_combo ada_maguro-userdebug
 add_lunch_combo ada_n7000-userdebug
 add_lunch_combo ada_tf101-userdebug
